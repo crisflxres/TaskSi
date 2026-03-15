a6cb8c1c081f60c98c7532827aab3fb8dc8a9c6c
Cristopher Armando Flores Bahena <cristopher_13240109235@uptecamac.edu.mx>
2026-03-14 23:45:54 -0600

    Lista local a base de datos SQLite y correccion del CRUD

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+import sqlite3
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app) #Permite que el Fronted (Interfaz) se conecte sin bloqueos de seguridad
 
-tareas = [] #BD
+DATABASE = "tareas.db" #BD
+
+def conectar_bd():
+    conexion  = sqlite3.connect(DATABASE)
+    conexion.row_factory = sqlite3.Row #Accede a los datos por nombre de columna
+    return conexion
+
+#Crea una tabla
+def crear_tabla():
+    conexion = conectar_bd()
+    cursor = conexion.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tareas (
+            id INTEGER PRIMARY KEY,
+            titulo TEXT NOT NULL,
+            descripcion TEXT,
+            completada BOOLEAN DEFAULT 0
+        )
+    ''')
+    conexion.commit()
+    conexion.close()
+    
+crear_tabla() #Ejecutamos la creacion al encender el servidor
 
 #Bienvenida
 @app.route('/', methods = ['GET'])
@@ -13,46 +35,109 @@ def bienvenida():
 
 #Leer todas las tareas
 @app.route('/tareas',methods =['GET'])
-def obtener_tarea():
-    return jsonify({"tareas": tareas, "total": len(tareas)})
+def obtener_tareas():
+    conexion = conectar_bd()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM tareas")
+    
+    #Convertir las filas de la BD en una lista de diccionarios
+    filas = cursor.fetchall()
+    lista_tareas = [dict(fila) for fila in filas]
+    conexion.close
+    return jsonify({"tareas": lista_tareas, "total": len(lista_tareas)})
 
 #Registrar
 @app.route('/tareas',methods = ['POST'])
 def agregar_tarea():
     nuevo = request.json
-    tareas.append(nuevo)
-    return jsonify({"mensaje":"Tarea Agregada"})
+    titulo = nuevo.get('titulo')
+    descripcion = nuevo.get('descripcion')
+    completada = nuevo.get('completada', False)
+    
+    #Validar que almenos este el titulo
+    if not titulo:
+        return jsonify({"Error": "El titulo es obligatorio"}), 400
+    
+    try:
+        conexion = conectar_bd()
+        cursor = conexion.cursor()
+        
+        #Insertamos los datos.El ID se genera en automatico
+        cursor.execute('''
+            INSERT INTO tareas (titulo, descripcion, completada) 
+            VALUES (?, ?, ?)
+        ''', (titulo, descripcion, completada))
+        
+        conexion.commit()
+        nuevo_id = cursor.lastrowid #Id generado por la BD
+        conexion.close
+        
+        return jsonify({
+            "mensaje": "Tarea agregada",
+            "id_asignado": nuevo_id
+        }),  201
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
 
 # Actualizar
 @app.route('/tareas/<int:id>', methods=['PUT'])
 def actualizar_tarea(id):
     datos_nuevos = request.json
     
-    for tarea in tareas:
-        # Verificamos que sea un diccionario y que el ID coincida
-        if isinstance(tarea, dict) and tarea.get('id') == id:
-            tarea['titulo'] = datos_nuevos.get('titulo', tarea.get('titulo', 'Sin título'))
-            tarea['descripcion'] = datos_nuevos.get('descripcion', tarea.get('descripcion', ''))
-            tarea['completada'] = datos_nuevos.get('completada', tarea.get('completada', False))
-            return jsonify({"mensaje": "Tarea actualizada", "tarea": tarea})
+    try:
+        conexion = conectar_bd()
+        cursor = conexion.cursor()
+        
+        #Verificamos que la tarea exista
+        cursor.execute("SELECT * FROM tareas WHERE id= ?", (id,))
+        tarea = cursor.fetchone()
+        
+        if tarea is None:
+            conexion.close()
+            return jsonify({"error": "No se encontro la tarea con ese ID"}), 404
             
-    return jsonify({"error": "No se encontro la tarea"}), 404
+        #Extraemos datos nuevos
+        nuevo_titulo = datos_nuevos.get('titulo', tarea ['titulo'])
+        nueva_descripcion = datos_nuevos.get('descripcion', tarea ['descripcion'])
+        nueva_comp = datos_nuevos.get('completada', tarea ['completada'])
+        
+        #Ejecutamos la acutalizacion de datos
+        cursor.execute('''
+                UPDATE tareas
+                SET titulo = ?, descripcion = ?, completada = ?
+                WHERE id= ?''',(nuevo_titulo, nueva_descripcion, nueva_comp, id))
+        
+        conexion.commit()
+        conexion.close()
+        
+        return jsonify({"mensaje": "Tarea actualizada existosamente,", "id": id})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 #Eliminar
 @app.route('/tareas/<int:id>',methods = ['DELETE'])
 def eliminar_tarea(id):
-    global tareas #Modificar la lista GLOBAL
     
-    #Existe la tarea
-    tarea_existe = any(isinstance(t, dict) and t.get('id') == id for t in tareas)
-    
-    if tarea_existe:
-        #Filtrar la lista menos la que tenga el ID buscado
-        tareas = [t for t in tareas if isinstance (t, dict) and t.get('id') != id]
+    try:
+        conexion = conectar_bd()
+        cursor = conexion.cursor()
+        
+        cursor.execute("DELETE FROM tareas WHERE id = ?", (id,))
+        
+        #cursor.rowcount me dice cuantas filas se borraron
+        
+        if cursor.rowcount == 0:
+            conexion.close()
+            return jsonify({"error": "No se encontro la tarea a eliminar"}), 404
+        conexion.commit()
+        conexion.close()
+        
         return jsonify({"mensaje": f"Tarea {id} eliminada correctamente"})
     
-    return jsonify ({"mensaje":"Tarea no encontrada"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
